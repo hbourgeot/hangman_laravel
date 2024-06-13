4e0ab8e09a18736b128cd0273fa025d801d630cc
henrrius <henrrybrgt@gmail.com>
2024-06-13 10:01:37 -0400

    now laravel serves angularjs views

--- public/web/app/app.module.js
+++ public/web/app/app.module.js
@@ -2,5 +2,7 @@
     "use strict";
     angular.module('myApp', []).controller('mainController', ['$scope',function ($scope) {
         $scope.greeting = 'Hola, Mundo!';
+    }]).config(['$locationProvider', function($locationProvider) {
+        $locationProvider.html5Mode(true);
     }]);
 })();
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('web/index.html'));
+})->where('any', '^(?!storage|web).*$');
